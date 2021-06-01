9c20d6720bfe323e239eae8d43182581c46a23d7
olmosmartin <tiincho18@gmail.com>
2021-06-01 18:01:05 -0300

    mejoradas las vistas y un agregado un form

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/notas-db',{
     useCreateIndex:true,
     useNewUrlParser:true,
-    useFindAndModify:false
+    useFindAndModify:false,
+    useUnifiedTopology: true
 })
 .then (db => console.log('conectado a la base de datos'))
 .catch(err=>console.error(err));
--- src/routes/notas.js
+++ src/routes/notas.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notas/agregar',(req, res)=>{
+    res.render('notas/agregar.ejs', {errors: [], titulo:"", descripcion:""});
+})
+
+router.post('/notas/nuevanota',(req, res)=>{
+    //console.log(req.body);
+    const {titulo, descripcion} = req.body;
+    const errors=[];
+    if(!titulo){
+        errors.push({text:"ingrese titulo"})
+    }
+    if(!descripcion){
+        errors.push({text:"ingrese descripcion"})
+    }
+    if(errors.length > 0){
+        console.log('errores')
+        res.render('notas/agregar.ejs', {errors: errors, titulo:titulo, descripcion:descripcion});
+    }else{
+        res.send('ok')
+    }
+})
+
 router.get('/notas',(req, res)=>{
     res.send("notas");
 })
